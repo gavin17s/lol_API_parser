4a096ca80e380386fd8f73cebb52f81626e121f5
Gavin S <gavin17s@users.noreply.github.com>
2016-01-26 15:05:39 +1300

    Fixed a bug
    Fixed a bug that occurs when looking up an unranked player.

--- riotAPIparser/subroutineAPI.py
+++ riotAPIparser/subroutineAPI.py
@@ -4,47 +4,55 @@ import urllib.request
 
 def getChampionName(championID, uRegion, uAPI):
 
-	with open("data/championDict.json") as jsonFileRead:
-		championDict = json.load(jsonFileRead)
+    with open("data/championDict.json") as jsonFileRead:
+        championDict = json.load(jsonFileRead)
 
-		championIDStr = str(championID)
+        championIDStr = str(championID)
 
-	if championIDStr not in championDict:
-		apiURL = "https://"+uRegion+".api.pvp.net/api/lol/static-data/"+uRegion+"/v1.2/champion/"+str(championID)+"?dataById=true&api_key="+uAPI
-		reader = codecs.getreader("utf-8")
-		nameResponse = urllib.request.urlopen(apiURL)
-		nameResponse = json.load(reader(nameResponse))
+    if championIDStr not in championDict:
+        apiURL = "https://"+uRegion+".api.pvp.net/api/lol/static-data/"+uRegion+"/v1.2/champion/"+str(championID)+"?dataById=true&api_key="+uAPI
+        reader = codecs.getreader("utf-8")
+        nameResponse = urllib.request.urlopen(apiURL)
+        nameResponse = json.load(reader(nameResponse))
 
-		championDict[championIDStr] = nameResponse["name"]
+        championDict[championIDStr] = nameResponse["name"]
 
-		with open("data/championDict.json", "w") as jsonFileWrite:
-			json.dump(championDict, jsonFileWrite)
+        with open("data/championDict.json", "w") as jsonFileWrite:
+            json.dump(championDict, jsonFileWrite)
 
-	return championDict[championIDStr]
+    return championDict[championIDStr]
 
 def requestPlayerData(pName, uRegion, uAPI):
 
-	apiURL = "https://"+uRegion+".api.pvp.net/api/lol/"+uRegion+"/v1.4/summoner/by-name/"+pName+"?api_key="+uAPI
+    apiURL = "https://"+uRegion+".api.pvp.net/api/lol/"+uRegion+"/v1.4/summoner/by-name/"+pName+"?api_key="+uAPI
 
-	reader = codecs.getreader("utf-8")
-	nameResponse = urllib.request.urlopen(apiURL)
-	nameResponse = json.load(reader(nameResponse))
-
-	return nameResponse
+    reader = codecs.getreader("utf-8")
+    
+    nameResponse = urllib.request.urlopen(apiURL)
+    nameResponse = json.load(reader(nameResponse))
+    
+    return nameResponse
 
 def requestPlayerRank(pID, uRegion, uAPI):
 
-	apiURL = "https://"+uRegion+".api.pvp.net/api/lol/"+uRegion+"/v2.5/league/by-summoner/"+pID+"/entry?api_key="+uAPI
+    apiURL = "https://"+uRegion+".api.pvp.net/api/lol/"+uRegion+"/v2.5/league/by-summoner/"+pID+"/entry?api_key="+uAPI
 
-	reader = codecs.getreader("utf-8")
-	rankResponse = urllib.request.urlopen(apiURL)
-	rankResponse = json.load(reader(rankResponse))
+    reader = codecs.getreader("utf-8")
+    
+    try:
+        rankResponse = urllib.request.urlopen(apiURL)
+        rankResponse = json.load(reader(rankResponse))
 
-	return rankResponse
+        return rankResponse
+    except:
+        return False
 
 def displayPlayerData(pID, pName, pRank):
 
-	print("\nPlayer {} is {} {} with {} LP.\n".format(pName, pRank[pID][0]['tier'], pRank[pID][0]['entries'][0]['division'], pRank[pID][0]['entries'][0]['leaguePoints']))
+    if pRank == False:
+        print("\nPlayer {}, unranked.\n".format(pName))
+    else:
+        print("\nPlayer {} is {} {} with {} LP.\n".format(pName, pRank[pID][0]['tier'], pRank[pID][0]['entries'][0]['division'], pRank[pID][0]['entries'][0]['leaguePoints']))
 
 def currentGame(pID, uRegion, uAPI):
 
@@ -62,28 +70,28 @@ def currentGame(pID, uRegion, uAPI):
 
 def displayCurrentGame(gameData, uRegion, uAPI):
 
-	with open("data/mapType.json") as jsonFileRead:
-			mapType = json.load(jsonFileRead)
+    with open("data/mapType.json") as jsonFileRead:
+        mapType = json.load(jsonFileRead)
 
-	with open("data/queueType.json") as jsonFileRead:
-			queueType = json.load(jsonFileRead)
+    with open("data/queueType.json") as jsonFileRead:
+        queueType = json.load(jsonFileRead)
 
-	if gameData == False:
-		print("Player currently not ingame.")
-	else:
-		mapStr = str(gameData["mapId"])
-		queueStr = str(gameData["gameQueueConfigId"])
-		print("Player currently ingame.\n\nMap: {}\nGame mode: {}\n".format(mapType[mapStr], queueType[queueStr]))
-		if gameData["gameQueueConfigId"] == 14 or gameData["gameQueueConfigId"] == 4 or gameData["gameQueueConfigId"] == 41 or gameData["gameQueueConfigId"] == 42:
-			print("Banned champions:")
-			for championB in gameData["bannedChampions"]:
-				print(getChampionName(championB["championId"], uRegion, uAPI))
+    if gameData == False:
+        print("Player currently not ingame.")
+    else:
+        mapStr = str(gameData["mapId"])
+        queueStr = str(gameData["gameQueueConfigId"])
+        print("Player currently ingame.\n\nMap: {}\nGame mode: {}\n".format(mapType[mapStr], queueType[queueStr]))
+        if gameData["gameQueueConfigId"] == 14 or gameData["gameQueueConfigId"] == 4 or gameData["gameQueueConfigId"] == 41 or gameData["gameQueueConfigId"] == 42:
+            print("Banned champions:")
+            for championB in gameData["bannedChampions"]:
+                print(getChampionName(championB["championId"], uRegion, uAPI))
 
-		print("\nBlue side:")
-		for summ in gameData["participants"]:
-			if summ["teamId"] == 100:
-				print(summ["summonerName"], "playing", getChampionName(summ["championId"], uRegion, uAPI))
-		print("\nRed side:")
-		for summ in gameData["participants"]:
-			if summ["teamId"] == 200:
-				print(summ["summonerName"], "playing", getChampionName(summ["championId"], uRegion, uAPI))
+        print("\nBlue side:")
+        for summ in gameData["participants"]:
+            if summ["teamId"] == 100:
+                print(summ["summonerName"], "playing", getChampionName(summ["championId"], uRegion, uAPI))
+        print("\nRed side:")
+        for summ in gameData["participants"]:
+            if summ["teamId"] == 200:
+                print(summ["summonerName"], "playing", getChampionName(summ["championId"], uRegion, uAPI))
